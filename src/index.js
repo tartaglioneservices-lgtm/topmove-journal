c0b4589eb64029f0fd5bb8249f35353e72273cdc
tartaglioneservices-lgtm <tartaglioneservices@gmail.com>
2025-11-10 10:04:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-
-// Service Worker pour PWA (optionnel mais recommandé)
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('Service Worker enregistré'))
-      .catch(err => console.log('Service Worker erreur:', err));
-  });
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
